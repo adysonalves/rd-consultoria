a8097aa9b8be3574c401b77df0639cec3b03012f
adysonalves <adyson6@gmail.com>
2023-06-09 00:58:11 -0300

    Seção Quem Somos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 const navMenu = document.getElementById("mobile-nav");
+const navMain = document.getElementById("nav-main");
 
-function alerta(){
-    alert("Fui clicado");
-}
+const navTop = navMain.offsetTop;
 
 function openToggle(){
     navMenu.style.display = "block"
@@ -11,3 +10,16 @@ function openToggle(){
 function closeToggle(){
     navMenu.style.display = "none"
 }
+
+function toggleMenuBackground() {
+    if (window.pageYOffset > navTop) {
+      navMain.classList.add("nav-background");
+    } else {
+      navMain.classList.remove("nav-background");
+    }
+  }
+  
+  // Adiciona um ouvinte de evento para a rolagem da janela
+  window.addEventListener("scroll", toggleMenuBackground);
+
+
