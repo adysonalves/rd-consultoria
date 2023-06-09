90a90c300ef56002706cd39f96c5951bc419e803
adysonalves <adyson6@gmail.com>
2023-06-08 23:18:59 -0300

    Menu Mobile adicionado

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const navMenu = document.getElementById("mobile-nav");
+
+function alerta(){
+    alert("Fui clicado");
+}
+
+function openToggle(){
+    navMenu.style.display = "block"
+}
+
+function closeToggle(){
+    navMenu.style.display = "none"
+}
