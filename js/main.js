45c2b699a851473ee726930cb6ca83b5b6ab7b76
adysonalves <adyson6@gmail.com>
2023-06-15 01:29:18 -0300

    Correções e Seção blog

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ window.addEventListener('load', function () {
   setTimeout(() => {
     loadingContainer.style.display = 'none';
     document.body.style.overflow = 'auto';
-  }, 1000)
+  }, 500)
 });
 
 
